2b85d6662f3f8b2d8f93dc33561376f92ab0b933
DomskieEe <152361897+DomskieEe@users.noreply.github.com>
2026-01-26 16:16:17 +0800

    Burial

--- db/StoredProcedures/SP_GeneralLedger.sql
+++ db/StoredProcedures/SP_GeneralLedger.sql
@@ -53,7 +53,7 @@ BEGIN
         FROM journalentryvoucher jev
         JOIN transactiontable trt ON TRIM(trt.LinkID) = TRIM(jev.LinkID)
         LEFT JOIN funds fnd ON fnd.ID = trt.FundsID
-        WHERE (trt.APAR LIKE '%Journal Entry Voucher%' OR trt.APAR LIKE '%JEV%')
+        WHERE (trt.APAR LIKE '%Journal Entry Voucher%' OR trt.APAR LIKE '%JEV%' OR trt.DocumentTypeID = 23)
           AND trt.Status IN ('Posted', 'approved', 'Approved')
           AND (REPLACE(jev.AccountCode, '-', '') LIKE @cleanMatch OR p_accountCode = '%')
           AND (CONCAT(trt.FundsID, '') LIKE p_fundID OR p_fundID = '%')
@@ -82,9 +82,63 @@ BEGIN
         JOIN transactionitems tri ON TRIM(tri.LinkID) = TRIM(trt.LinkID)
         JOIN chartofaccounts coa ON coa.ID = tri.ChargeAccountID
         LEFT JOIN funds fnd ON fnd.ID = trt.FundsID
-        WHERE (trt.APAR LIKE '%Disbursement Voucher%' OR trt.APAR LIKE '%DV%')
+        WHERE (trt.APAR LIKE '%Disbursement Voucher%' OR trt.APAR LIKE '%DV%' OR trt.DocumentTypeID IN (4, 5))
           AND trt.Status IN ('Posted', 'approved', 'Approved')
           AND (REPLACE(coa.AccountCode, '-', '') LIKE @cleanMatch OR p_accountCode = '%')
+          AND (CONCAT(trt.FundsID, '') LIKE p_fundID OR p_fundID = '%')
+          AND DATE(trt.InvoiceDate) <= DATE(p_cutoff)
+
+        UNION ALL
+
+        -- Part 3: Burial & Marriage Receipts (Debit side: 1-01-01-010)
+        SELECT 
+            trt.ID AS id,
+            trt.ID AS transaction_id,
+            trt.LinkID AS link_id,
+            trt.APAR AS ap_ar,
+            IFNULL(fnd.Name, 'N/A') AS fund,
+            'Cash - Local Treasury' AS account_name,
+            '1-01-01-010' AS account_code,
+            trt.InvoiceDate AS date,
+            CONCAT(trt.APAR, ': ', IFNULL(trt.CustomerName, '')) AS ledger_item,
+            trt.InvoiceNumber AS invoice_number,
+            p_accountCode AS account_code_display,
+            'Cash - Local Treasury' AS account_name_display,
+            trt.Debit AS debit,
+            0.00 AS credit,
+            '' AS municipality
+        FROM transactiontable trt
+        LEFT JOIN funds fnd ON fnd.ID = trt.FundsID
+        WHERE (trt.APAR LIKE '%Burial Receipt%' OR trt.APAR LIKE '%Marriage Receipt%' OR trt.DocumentTypeID IN (18, 19))
+          AND trt.Status IN ('Posted', 'Approved', 'approved')
+          AND ('10101010' LIKE @cleanMatch OR p_accountCode = '%')
+          AND (CONCAT(trt.FundsID, '') LIKE p_fundID OR p_fundID = '%')
+          AND DATE(trt.InvoiceDate) <= DATE(p_cutoff)
+
+        UNION ALL
+
+        -- Part 4: Burial & Marriage Receipts (Credit side: 1-01-01-020)
+        SELECT 
+            trt.ID + 1000000 AS id, 
+            trt.ID AS transaction_id,
+            trt.LinkID AS link_id,
+            trt.APAR AS ap_ar,
+            IFNULL(fnd.Name, 'N/A') AS fund,
+            'Petty Cash' AS account_name,
+            '1-01-01-020' AS account_code,
+            trt.InvoiceDate AS date,
+            CONCAT(trt.APAR, ': ', IFNULL(trt.CustomerName, '')) AS ledger_item,
+            trt.InvoiceNumber AS invoice_number,
+            p_accountCode AS account_code_display,
+            'Petty Cash' AS account_name_display,
+            0.00 AS debit,
+            trt.Credit AS credit,
+            '' AS municipality
+        FROM transactiontable trt
+        LEFT JOIN funds fnd ON fnd.ID = trt.FundsID
+        WHERE (trt.APAR LIKE '%Burial Receipt%' OR trt.APAR LIKE '%Marriage Receipt%' OR trt.DocumentTypeID IN (18, 19))
+          AND trt.Status IN ('Posted', 'Approved', 'approved')
+          AND ('10101020' LIKE @cleanMatch OR p_accountCode = '%')
           AND (CONCAT(trt.FundsID, '') LIKE p_fundID OR p_fundID = '%')
           AND DATE(trt.InvoiceDate) <= DATE(p_cutoff)
 

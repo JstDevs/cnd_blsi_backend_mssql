1df57cda7c64b1c579ab2741cd99827d3cb14879
DomskieEe <152361897+DomskieEe@users.noreply.github.com>
2026-02-03 10:14:57 +0800

    subsi ledger

--- db/StoredProcedures/SP_SubsidiaryLedger.sql
+++ db/StoredProcedures/SP_SubsidiaryLedger.sql
@@ -9,22 +9,22 @@ CREATE PROCEDURE SP_SubsidiaryLedger (
 )
 BEGIN
     SELECT 
-        gle.ID,
-        fnd.Name AS Fund,
-        gle.AccountName,
-        gle.AccountCode,
-        DATE(gle.CreatedDate) AS Date,
-        gle.LedgerItem,
+        gle.ID AS id,
+        fnd.Name AS fund,
+        gle.AccountName AS account_name,
+        gle.AccountCode AS account_code,
+        DATE(gle.CreatedDate) AS date,
+        gle.LedgerItem AS ledger_item,
         CASE
             WHEN IFNULL(gle.Debit, 0) = 0.00 THEN NULL
             ELSE gle.Debit
-        END AS Debit,
+        END AS debit,
         CASE
             WHEN IFNULL(gle.Credit, 0) = 0.00 THEN NULL
             ELSE gle.Credit
-        END AS Credit,
-        SUM(IFNULL(gle.Debit, 0) - IFNULL(gle.Credit, 0)) OVER (ORDER BY gle.CreatedDate, gle.ID) AS Balance,
-        lmu.Name AS Municipality
+        END AS credit,
+        SUM(IFNULL(gle.Debit, 0) - IFNULL(gle.Credit, 0)) OVER (ORDER BY gle.CreatedDate, gle.ID) AS balance,
+        lmu.Name AS municipality
     FROM generalledger AS gle
     INNER JOIN funds AS fnd ON fnd.ID = gle.FundID
     INNER JOIN lgu AS lgu ON lgu.ID = 1
